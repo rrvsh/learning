2da2f92eaf52bacc2aa9c9c339aa1a01c5a6be43
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-21 02:50:01 +0800

    feat(fizzbuzz): add bazz

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,10 +1,11 @@
-for n in range(100):
-    numberToEvaluate = n + 1
+for numberToEvaluate in range(1, 101): # 101 is inclusive
     buffer = ""
     if numberToEvaluate % 3 == 0:
         buffer += "Fizz"
     if numberToEvaluate % 5 == 0:
         buffer += "Buzz"
+    if numberToEvaluate % 7 == 0:
+        buffer = "Bazz" # We overwrite here since we don't want to combine it.
     if buffer == "":
         print numberToEvaluate
     else:
