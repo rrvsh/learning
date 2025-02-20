3a2c7e1497d6b32bc0aaa8557af0a000174732c9
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-21 02:35:37 +0800

    refactor(fizzbuzz): made python use buffer

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,10 +1,11 @@
 for n in range(100):
     numberToEvaluate = n + 1
-    if numberToEvaluate % 3 == 0 and numberToEvaluate % 5 == 0:
-        print "FizzBuzz"
-    elif numberToEvaluate % 3 == 0:
-        print "Fizz"
-    elif numberToEvaluate % 5 == 0:
-        print "Buzz"
-    else:
+    buffer = ""
+    if numberToEvaluate % 3 == 0:
+        buffer += "Fizz"
+    if numberToEvaluate % 5 == 0:
+        buffer += "Buzz"
+    if buffer == "":
         print numberToEvaluate
+    else:
+        print buffer
