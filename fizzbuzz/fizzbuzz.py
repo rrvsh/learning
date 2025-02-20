66e2db75e3b3d7b5dccd3266a49a35835a8f75aa
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-02-21 02:30:59 +0800

    refactor(fizzbuzz): made python nest less

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,8 +1,8 @@
 for n in range(100):
     numberToEvaluate = n + 1
-    if numberToEvaluate % 3 == 0:
-        if numberToEvaluate % 5 == 0:
-            print "FizzBuzz"
+    if numberToEvaluate % 3 == 0 and numberToEvaluate % 5 == 0:
+        print "FizzBuzz"
+    elif numberToEvaluate % 3 == 0:
         print "Fizz"
     elif numberToEvaluate % 5 == 0:
         print "Buzz"
