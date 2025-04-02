4c3dcb6047c3cd1123737d524a967b5eeabcea41
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-04-02 13:47:30 +0800

    feat(flask): try out variables

--- python/flask/app.py
+++ python/flask/app.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello World!</p>"
+@app.get("/")
+def home():
+    return "home"
+
+
+@app.route("/<username>")
+def hello(username):
+    # Use escape for user input to protect against injection attacks
+    return f"hello, {escape(username)}"
