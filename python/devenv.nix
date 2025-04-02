e3602c79a341e5f08b7233c9b52c35da0dde57ff
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-04-02 11:56:43 +0800

    chore: rm nonessential pkgs from python

--- python/devenv.nix
+++ python/devenv.nix
@@ -2,13 +2,5 @@
 {
   languages.python = {
     enable = true;
-    version = "3.9";
-    venv.enable = true;
-    venv.requirements = ''
-      win2xcur
-    '';
   };
-  packages = [
-    pkgs.python39Packages.wand
-  ];
 }
