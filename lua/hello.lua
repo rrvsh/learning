6edc89f59d34331ec9530fc412fb257550ab0211
Mohammad Rafiq <rafiq@rrv.sh>
2025-09-09 03:02:38 +0800

    feat(lua): conditionals and loops

--- lua/hello.lua
+++ lua/hello.lua
@@ -1,1 +1,16 @@
 print("Hello, world!")
+
+local num = 38
+local s = "not other"
+while num < 50 do
+  num = num + 1
+  if num > 40 then
+    print("over 40")
+  elseif s ~= "other" then
+    print("not over 40")
+    s = "other"
+  else
+    local line = io.read()
+    print("hello " .. line)
+  end
+end
