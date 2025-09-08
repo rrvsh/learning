04eb908a31ef99b41e2d76ff4024fc96669e0b4a
Mohammad Rafiq <rafiq@rrv.sh>
2025-09-09 03:07:10 +0800

    feat(lua): function

--- lua/hello.lua
+++ lua/hello.lua
@@ -8,9 +8,23 @@ while num < 50 do
     print("over 40")
   elseif s ~= "other" then
     print("not over 40")
-    s = "other"
+    -- s = "other"
   else
     local line = io.read()
     print("hello " .. line)
   end
+end
+
+print("")
+
+local function fib(n)
+  if n < 2 then return 1 end
+  return fib(n-1) + fib(n-2)
+end
+
+print("Fibonacci Sequence 0 <= n <= 10")
+local n = 0
+while n <= 10 do
+  print(fib(n))
+  n = n + 1
 end
