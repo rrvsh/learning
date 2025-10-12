bf0bbeaddf6153907378c765ac80e8511a498feb
Mohammad Rafiq <rafiq@rrv.sh>
2025-10-12 15:10:15 +0800

    build(uv2nix): init devshell flake

---
+++ python/uv2nix/flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              python3
+              uv
+            ];
+          };
+        };
+    };
+}
